3bcd3a8e065ef394b79ef70e0119ca758f6b22dc
Cmejia49 <mejiachristopher49@gmail.com>
2023-03-26 00:57:14 +0800

    add list form

--- TodaList/src/app/todo-list/todo-list.module.ts
+++ TodaList/src/app/todo-list/todo-list.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './card/card.component';
+import { ListFormComponent } from './list-form/list-form.component';
 
 
 
 @NgModule({
   declarations: [
-    CardComponent
+    CardComponent,
+    ListFormComponent
   ],
   imports: [
     CommonModule
   ],
   exports:[
-    CardComponent
+    ListFormComponent
   ]
 })
 export class TodoListModule { }
